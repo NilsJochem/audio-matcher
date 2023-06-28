627332177cc183dc9a99ff4be804ff8533dfdabe
nilsj <nils.jochem@gmail.com>
2023-06-28 14:12:05 +0200

    added defaults to structs added tests added frame_iter

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,11 @@ use std::time::Duration;
 mod progress_bar {
     use pad::PadStr;
     use std::io::{stdout, Write};
-    pub(crate) struct Arrow<'a> {
-        pub(crate) arrow_prefix: &'a str,
-        pub(crate) arrow_suffix: &'a str,
-        pub(crate) arrow_char: char,
-        pub(crate) arrow_tip: char,
+    pub struct Arrow<'a> {
+        pub arrow_prefix: &'a str,
+        pub arrow_suffix: &'a str,
+        pub arrow_char: char,
+        pub arrow_tip: char,
     }
 
     impl Default for Arrow<'_> {
@@ -25,18 +25,21 @@ mod progress_bar {
 
     impl Arrow<'_> {
         fn build(&self, fraction: f64, bar_length: usize) -> String {
+            let fraction = fraction.clamp(0.0, 1.0);
+
             let mut arrow = String::new();
             arrow.push_str(self.arrow_prefix);
-
             arrow.push_str(
                 &self
                     .arrow_char
                     .to_string()
-                    .repeat(((fraction * bar_length as f64) as usize).saturating_sub(1)),
+                    .repeat((fraction * bar_length as f64).round() as usize),
             );
-            arrow.push(self.arrow_tip);
+            if bar_length - (arrow.len() - self.arrow_prefix.len()) > 0 {
+                arrow.push(self.arrow_tip);
+            }
             arrow.push_str(
-                " ".repeat(self.arrow_prefix.len() + bar_length - arrow.len())
+                " ".repeat(bar_length - (arrow.len() - self.arrow_prefix.len()))
                     .as_str(),
             );
             arrow.push_str(self.arrow_suffix);
@@ -44,30 +47,25 @@ mod progress_bar {
         }
     }
 
-    pub(crate) struct ProgressBar<'a> {
-        pub(crate) total: usize,
-        pub(crate) bar_length: usize,
-        pub(crate) pre_msg: &'a str,
-        pub(crate) arrow: Arrow<'a>,
+    pub struct ProgressBar<'a> {
+        pub bar_length: usize,
+        pub pre_msg: &'a str,
+        pub arrow: Arrow<'a>,
+    }
+
+    impl Default for ProgressBar<'_> {
+        fn default() -> Self {
+            Self {
+                bar_length: 20,
+                pre_msg: "Progress: ",
+                arrow: Arrow::default(),
+            }
+        }
     }
 
     impl ProgressBar<'_> {
-        pub(crate) fn new<'a>(
-            total: usize,
-            bar_length: Option<usize>,
-            pre_msg: Option<&'a str>,
-            arrow: Arrow<'a>,
-        ) -> ProgressBar<'a> {
-            ProgressBar {
-                total,
-                bar_length: bar_length.unwrap_or(20),
-                pre_msg: pre_msg.unwrap_or("'Progress: '"),
-                arrow,
-            }
-        }
-
-        pub(crate) fn print_bar(&self, current: usize, post_msg: &str) {
-            let total = self.total.max(current);
+        pub fn print_bar(&self, current: usize, total: usize, post_msg: &str) {
+            let total = total.max(current);
             let fraction = current as f64 / total as f64;
 
             let current_fmt = current.to_string().pad(
@@ -90,34 +88,63 @@ mod progress_bar {
             stdout().flush().unwrap();
         }
     }
+
+    #[cfg(test)]
+    mod tests {
+        use super::*;
+
+        #[test]
+        fn empty_arrow() {
+            assert_eq!(
+                Arrow::default().build(0.0, 10),
+                String::from("[>         ]")
+            )
+        }
+        #[test]
+        fn short_arrow() {
+            assert_eq!(
+                Arrow::default().build(0.2, 10),
+                String::from("[==>       ]")
+            )
+        }
+        #[test]
+        fn long_arrow() {
+            assert_eq!(
+                Arrow::default().build(0.9, 10),
+                String::from("[=========>]")
+            )
+        }
+        #[test]
+        fn full_arrow() {
+            assert_eq!(
+                Arrow::default().build(1.0, 10),
+                String::from("[==========]")
+            )
+        }
+    }
 }
 
 fn offset_range(range: &std::ops::Range<usize>, offset: usize) -> std::ops::Range<usize> {
     (range.start + offset)..(range.end + offset)
 }
 
-#[test]
-fn chunked_test() {
-    let is = chunked((1..5).into_iter(), 3, 2).collect_vec();
-    let expected = vec![vec![1, 2, 3], vec![3, 4]];
-    assert!(
-        &is.eq(&expected),
-        "expected {:?} but was {:?}",
-        expected,
-        is
-    );
-
-    let is = chunked((0..15).into_iter(), 6, 4).collect_vec();
-    let expected = vec![0..6, 4..10, 8..14, 12..15]
-        .into_iter()
-        .map(|r| r.collect_vec())
-        .collect_vec();
-    assert!(
-        &is.eq(&expected),
-        "expected {:?} but was {:?}",
-        expected,
-        is
-    );
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn chunked_test() {
+        let is = chunked((0..15).into_iter(), 6, 4).collect_vec();
+        let expected = vec![0..6, 4..10, 8..14, 12..15]
+            .into_iter()
+            .map(|r| r.collect_vec())
+            .collect_vec();
+        assert!(
+            &is.eq(&expected),
+            "expected {:?} but was {:?}",
+            expected,
+            is
+        );
+    }
 }
 
 fn chunked<T: Clone>(
@@ -125,7 +152,7 @@ fn chunked<T: Clone>(
     window_size: usize,
     hop_length: usize,
 ) -> impl Iterator<Item = Vec<T>> {
-    let mut buffer = Vec::new();
+    let mut buffer = Vec::with_capacity(hop_length);
     std::iter::from_fn(move || {
         while buffer.len() < window_size {
             match data.next() {
@@ -164,15 +191,25 @@ fn print_offsets(peaks: &Vec<find_peaks::Peak<f64>>, sr: u16) {
     }
 }
 
+fn open_file_or_exit(snippet_path: &str) -> File {
+    match File::open(snippet_path) {
+        Ok(file) => file,
+        Err(_) => {
+            println!("couldn't find file '{snippet_path}'");
+            std::process::exit(1);
+        }
+    }
+}
+
 fn main() {
     let snippet_path = "res/Interlude.mp3";
     let main_path = "res/big_test.mp3";
 
     println!("preparing data");
-    let snippet = File::open(snippet_path).expect("couldn't find snippet file");
+    let snippet = open_file_or_exit(snippet_path);
     let (s_header, s_samples) = mp3_reader::read_mp3(snippet).expect("invalid snippet mp3");
 
-    let main_data = File::open(main_path).expect("couln't find main data file");
+    let main_data = open_file_or_exit(main_path);
     let (m_header, m_samples) = mp3_reader::read_mp3(main_data).expect("invalid main data mp3");
     println!("prepared data");
 
@@ -200,118 +237,174 @@ fn main() {
 }
 
 mod mp3_reader {
-    use minimp3::Decoder;
+    use itertools::Itertools;
+    use minimp3::{Decoder, Frame};
     use std::{fs::File, time::Duration};
 
     // because all samples are 16 bit usage of a single factor is adequat
     const PCM_FACTOR: f64 = 1.0 / (1 << 16 - 1) as f64;
-    pub(crate) fn read_mp3(file: File) -> Result<(u16, impl Iterator<Item = f64>), minimp3::Error> {
-        let mut decoder = Decoder::new(file);
-        let mut frame = decoder.next_frame()?;
+    pub fn read_mp3(file: File) -> Result<(u16, impl Iterator<Item = f64>), minimp3::Error> {
+        let (sample_rate, iter) = frame_iterator(Decoder::new(file))?;
 
-        let sr = frame.sample_rate as u16;
-        let mut i = 0;
-
-        let iter = std::iter::from_fn(move || {
-            if i >= frame.data.len() {
-                frame = match decoder.next_frame() {
-                    Ok(frame) => {
-                        if frame.sample_rate as u16 != sr {
-                            panic!("sample rate changed")
-                        }
-                        frame
-                    }
-                    Err(minimp3::Error::Eof) => return None,
-                    Err(e) => panic!("{:?}", e),
-                };
-                i = 0;
+        let iter = iter.flat_map(move |frame| {
+            if frame.sample_rate as u16 != sample_rate {
+                panic!("sample rate changed")
             }
-            let sample = match frame.channels {
-                1 => frame.data[i] as f64,
-                2 => (frame.data[i] as i32 + frame.data[i + 1] as i32) as f64 * 0.5,
-                x => panic!("can't handle {x} channels"),
-            } * PCM_FACTOR;
-            i += frame.channels;
-            Some(sample)
+            if frame.channels != 2 {
+                panic!("can only handle stereo")
+            }
+            frame
+                .data
+                .iter()
+                .chunks(2)
+                .into_iter()
+                .map(|c| {
+                    let (l, r) = c.collect_tuple().unwrap();
+                    (*l as f64 + *r as f64) * 0.5 * PCM_FACTOR
+                })
+                .collect::<Vec<_>>()
         });
-        Ok((sr, iter))
+
+        Ok((sample_rate, iter))
     }
 
-    pub(crate) fn mp3_duration(path: &str) -> std::io::Result<Duration> {
+    struct Wrapper(Decoder<File>);
+
+    impl Iterator for Wrapper {
+        type Item = Frame;
+        fn next(&mut self) -> Option<Self::Item> {
+            match self.0.next_frame() {
+                Ok(frame) => Some(frame),
+                Err(minimp3::Error::Eof) => None,
+                Err(e) => panic!("{:?}", e),
+            }
+        }
+    }
+
+    fn frame_iterator(
+        mut decoder: Decoder<File>,
+    ) -> Result<(u16, impl Iterator<Item = Frame>), minimp3::Error> {
+        let first_frame = decoder.next_frame()?;
+        let sample_rate = first_frame.sample_rate as u16;
+
+        Ok((
+            sample_rate,
+            [first_frame].into_iter().chain(Wrapper(decoder)),
+        ))
+    }
+
+    pub fn mp3_duration(path: &str) -> std::io::Result<Duration> {
         // first try external bibliothek
         if let Ok(duration) = mp3_duration::from_path(path) {
             return Ok(duration);
         }
         let file = File::open(path)?;
-        let mut decoder = Decoder::new(file);
-        let mut seconds = 0.0;
-        while let Ok(frame) = decoder.next_frame() {
-            seconds += frame.data.len() as f64 / (frame.channels as f64 * frame.sample_rate as f64)
-        }
+
+        let decoder = Decoder::new(file);
+        let (_, frames) = frame_iterator(decoder).unwrap();
+        let seconds: f64 = frames
+            // .par_bridge() // parrallel, but seems half as fast
+            .map(|frame| {
+                frame.data.len() as f64 / (frame.channels as f64 * frame.sample_rate as f64)
+            })
+            .sum();
         Ok(Duration::from_secs_f64(seconds))
+    }
+
+    #[cfg(test)]
+    mod tests {
+        use super::*;
+
+        #[test]
+        fn short_mp3_duration() {
+            assert_eq!(mp3_duration("res/Interlude.mp3").unwrap().as_secs(), 7);
+        }
+        #[test]
+        #[ignore = "slow"]
+        fn long_mp3_duration() {
+            assert_eq!(
+                mp3_duration("res/big_test.mp3").unwrap().as_secs(),
+                (3 * 60 + 20) * 60 + 55
+            );
+        }
+
+        #[test]
+        fn short_mp3_samples() {
+            assert_eq!(
+                read_mp3(File::open("res/Interlude.mp3").unwrap())
+                    .unwrap()
+                    .1
+                    .count(),
+                323712
+            )
+        }
+
+        #[test]
+        #[ignore = "slow"]
+        fn long_mp3_samples() {
+            assert_eq!(
+                read_mp3(File::open("res/big_test.mp3").unwrap())
+                    .unwrap()
+                    .1
+                    .count(),
+                531668736
+            )
+        }
     }
 }
 
 mod audio_matcher {
-    use crate::{offset_range, chunked};
-    use crate::progress_bar::{ProgressBar, Arrow};
-    use std::time::Duration;
+    use crate::progress_bar::ProgressBar;
+    use crate::{chunked, offset_range};
     use fftconvolve::fftcorrelate;
     use find_peaks::PeakFinder;
+    use itertools::Itertools;
     use ndarray::Array1;
+    use std::time::Duration;
 
-    pub(crate) fn calc_chunks(
+    pub fn calc_chunks(
         sr: u16,
         m_samples: impl Iterator<Item = f64> + 'static,
         s_samples: impl Iterator<Item = f64>,
         chunk_size: Duration,
         overlap_length: Duration,
-        duration: Duration,
+        m_duration: Duration,
         distance: Duration,
         prominence: f64,
     ) -> Vec<find_peaks::Peak<f64>> {
         // normalize inputs
-        let chunk_size = chunk_size.as_secs() * sr as u64;
-        let overlap_length = overlap_length.as_secs() * sr as u64;
-        let chunks = (duration.as_secs() as f64 * sr as f64 / chunk_size as f64).ceil() as usize;
+        let chunks = ((m_duration).as_secs_f64() / chunk_size.as_secs_f64()).ceil() as usize;
+        let overlap_length = (overlap_length.as_secs_f64() * sr as f64).round() as u64;
+        let chunk_size = (chunk_size.as_secs_f64() * sr as f64).round() as u64;
 
         let s_samples: Array1<f64> = Array1::from_iter(s_samples);
-        let progress_bar = ProgressBar::new(
-            chunks,
-            Some(chunks.min(80)),
-            None,
-            Arrow {
-                arrow_char: '-',
-                arrow_tip: '-',
-                ..Default::default()
-            },
-        );
+        let progress_bar = ProgressBar {
+            bar_length: chunks.min(80),
+            ..ProgressBar::default()
+        };
 
-        let mut peaks = Vec::new();
-        for (i, chunk) in chunked(
+        chunked(
             m_samples,
             chunk_size as usize + overlap_length as usize,
             chunk_size as usize,
         )
         .enumerate()
-        {
-            progress_bar.print_bar(i + 1, "");
+        .flat_map(|(i, chunk)| {
+            progress_bar.print_bar(i, chunks - 1, "");
             let offset = chunk_size as usize * i;
             let m_samples = Array1::from_iter(chunk.into_iter());
             let _matches = fftcorrelate(&m_samples, &s_samples, fftconvolve::Mode::Valid)
                 .unwrap()
                 .to_vec();
-            peaks.extend(
-                find_peaks(&_matches, sr, distance, prominence)
-                    .iter()
-                    .map(|p| {
-                        let mut p = p.clone();
-                        p.position = offset_range(&p.position, offset);
-                        p
-                    }),
-            );
-        }
-        peaks
+            find_peaks(&_matches, sr, distance, prominence)
+                .iter()
+                .map(|p| {
+                    let mut p = p.clone();
+                    p.position = offset_range(&p.position, offset);
+                    p
+                }).collect::<Vec<_>>()
+        })
+        .collect_vec()
     }
 
     fn find_peaks(
@@ -325,5 +418,55 @@ mod audio_matcher {
         fp.with_min_distance(distance.as_secs() as usize * sr as usize);
         let peaks = fp.find_peaks();
         peaks
+    }
+
+    #[cfg(test)]
+    mod tests {
+        use itertools::Itertools;
+
+        use super::*;
+
+        #[test]
+        #[ignore = "slow"]
+        fn short_calc_peaks() {
+            let snippet_path = "res/Interlude.mp3";
+            let main_path = "res/small_test.mp3";
+
+            println!("preparing data");
+            let snippet = std::fs::File::open(snippet_path).unwrap();
+            let (s_header, s_samples) =
+                crate::mp3_reader::read_mp3(snippet).expect("invalid snippet mp3");
+
+            let main_data = std::fs::File::open(main_path).unwrap();
+            let (m_header, m_samples) =
+                crate::mp3_reader::read_mp3(main_data).expect("invalid main data mp3");
+            println!("prepared data");
+
+            if s_header != m_header {
+                panic!("sample rate dosn't match")
+            }
+            let sr = s_header;
+
+            let n =
+                crate::mp3_reader::mp3_duration(main_path).expect("couln't refind main data file");
+            println!("got duration");
+            let peaks = calc_chunks(
+                sr,
+                m_samples,
+                s_samples,
+                Duration::from_secs(2 * 60),
+                crate::mp3_reader::mp3_duration(snippet_path)
+                    .expect("couln't refind snippet data file")
+                    / 2,
+                n,
+                Duration::from_secs(5 * 60),
+                250.,
+            );
+            assert!(peaks
+                .into_iter()
+                .map(|p| p.position.start / sr as usize)
+                .sorted()
+                .eq(vec![21, 16 * 60 + 43]));
+        }
     }
 }
