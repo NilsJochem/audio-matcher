c2529d2930adb8ea27b8b6026d0a86b21b6f661b
nilsj <nils.jochem@gmail.com>
2023-07-01 20:11:53 +0200

    added benchmark to compare chunk-sizes

--- benches/my_benchmark.rs
+++ benches/my_benchmark.rs
@@ -1,17 +1,29 @@
+use std::time::Duration;
+
 use audio_matcher::args::Arguments;
 use clap::Parser;
-use criterion::{black_box, criterion_group, criterion_main, Criterion, BenchmarkId};
+use criterion::{black_box, criterion_group, criterion_main, BenchmarkId, Criterion};
 
 fn full_match(c: &mut Criterion) {
     // c.bench_function("fib 20", |b| b.iter(|| fibonacci(black_box(20))));
-	let input = Arguments::parse_from(["", "res/local/small_test.mp3", "--snippet", "res/local/Interlude.mp3", "--no-out", "--dry-run", "-n"]);
-	c.bench_with_input(BenchmarkId::new("peaks in small_test", ""), &input, |b, args|{
-		b.iter(||audio_matcher::run(black_box(args.clone())))
-	});
+    let input = Arguments::parse_from([
+        "",
+        "res/local/small_test.mp3",
+        "--snippet",
+        "res/local/Interlude.mp3",
+        "--no-out",
+        "--dry-run",
+        "-n",
+    ]);
+    c.bench_with_input(
+        BenchmarkId::new("peaks in small_test", ""),
+        &input,
+        |b, args| b.iter(|| audio_matcher::run(black_box(args.clone()))),
+    );
 }
 
 fn correlate_vs_bib(c: &mut Criterion) {
-	let mut planner = realfft::RealFftPlanner::new();
+    let mut planner = realfft::RealFftPlanner::new();
 
     let mode = audio_matcher::audio_matcher::Mode::Valid;
     let data1: Vec<f32> = audio_matcher::audio_matcher::test_data(100..150);
@@ -20,10 +32,17 @@ fn correlate_vs_bib(c: &mut Criterion) {
 
     group.bench_function("correlate my func", |b| {
         b.iter(|| {
-            audio_matcher::audio_matcher::correlate(&mut planner, black_box(&data2), black_box(&data1), black_box(&mode), false, true)
-                .unwrap()
-				.as_ref()
-                .to_vec()
+            audio_matcher::audio_matcher::correlate(
+                &mut planner,
+                black_box(&data2),
+                black_box(&data1),
+                black_box(&mode),
+                false,
+                true,
+            )
+            .unwrap()
+            .as_ref()
+            .to_vec()
         })
     });
     group.bench_function("correlate old func", |b| {
@@ -37,11 +56,11 @@ fn correlate_vs_bib(c: &mut Criterion) {
             .to_vec()
         })
     });
-	group.finish();
+    group.finish();
 }
 
 fn correlate_vs_conj(c: &mut Criterion) {
-	let mut planner = realfft::RealFftPlanner::new();
+    let mut planner = realfft::RealFftPlanner::new();
 
     let mode = audio_matcher::audio_matcher::Mode::Valid;
     let data1: Vec<f32> = audio_matcher::audio_matcher::test_data(100..150);
@@ -51,22 +70,62 @@ fn correlate_vs_conj(c: &mut Criterion) {
 
     group.bench_function("correlate my func + conjugate", |b| {
         b.iter(|| {
-            audio_matcher::audio_matcher::correlate(&mut planner, black_box(&data2), black_box(&data1), black_box(&mode), false, true)
-                .unwrap()
-				.as_ref()
-                .to_vec()
+            audio_matcher::audio_matcher::correlate(
+                &mut planner,
+                black_box(&data2),
+                black_box(&data1),
+                black_box(&mode),
+                false,
+                true,
+            )
+            .unwrap()
+            .as_ref()
+            .to_vec()
         })
     });
     group.bench_function("correlate my func + reverse mult", |b| {
         b.iter(|| {
-            audio_matcher::audio_matcher::correlate(&mut planner, black_box(&data2), black_box(&data1), black_box(&mode), false, false)
-                .unwrap()
-				.as_ref()
-                .to_vec()
+            audio_matcher::audio_matcher::correlate(
+                &mut planner,
+                black_box(&data2),
+                black_box(&data1),
+                black_box(&mode),
+                false,
+                false,
+            )
+            .unwrap()
+            .as_ref()
+            .to_vec()
         })
     });
-	group.finish();
+    group.finish();
 }
 
-criterion_group!(benches, full_match, correlate_vs_bib, correlate_vs_conj);
+fn full_match_duration_vs(c: &mut Criterion) {
+    let mut group = c.benchmark_group("compare_chunk_sizes");
+	group.sample_size(10);
+	group.measurement_time(Duration::from_secs(60));
+    let input = Arguments::parse_from([
+        "",
+        "res/local/small_test.mp3",
+        "--snippet",
+        "res/local/Interlude.mp3",
+        "--no-out",
+        "--dry-run",
+        "-n",
+    ]);
+    for distance in [8, 20, 60, 120] {
+        let mut input = input.clone();
+        input.distance = distance;
+        group.bench_with_input(
+            BenchmarkId::new("peaks in small_test", distance),
+            &input,
+            |b, args| b.iter(|| audio_matcher::run(black_box(args.clone()))),
+        );
+    }
+
+    group.finish();
+}
+
+criterion_group!(benches, full_match, correlate_vs_bib, correlate_vs_conj, full_match_duration_vs);
 criterion_main!(benches);
