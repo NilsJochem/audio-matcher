cf93dcc17643d7994f5cc7913d53db1b4baf676f
nilsj <nils.jochem@gmail.com>
2023-07-22 18:46:52 +0200

    added new clippy warnings

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,19 @@
-#![warn(clippy::nursery, clippy::pedantic)]
+#![warn(
+    clippy::nursery,
+    clippy::pedantic,
+    clippy::empty_structs_with_brackets,
+    clippy::format_push_string,
+    clippy::if_then_some_else_none,
+    clippy::impl_trait_in_params,
+    clippy::missing_assert_message,
+    clippy::multiple_inherent_impl,
+    clippy::non_ascii_literal,
+    clippy::self_named_module_files,
+    clippy::semicolon_inside_block,
+    clippy::separated_literal_suffix,
+    clippy::str_to_string,
+    clippy::string_to_string
+)]
 #![allow(
     clippy::cast_possible_truncation,
     clippy::cast_precision_loss,
