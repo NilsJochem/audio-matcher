d5c63bd17abfc94136d3c03bc7b6554d83c9576b
nilsj <nils.jochem@gmail.com>
2023-12-04 14:29:04 +0100

    added DurationDisplay

--- common/src/extensions/duration.rs
+++ common/src/extensions/duration.rs
@@ -8,6 +8,9 @@ pub trait Ext {
     fn minutes(&self) -> u64;
     /// returns the seconds represented by this `self`
     fn seconds(&self) -> u64;
+
+    /// retursn a displayable Version of Duration
+    fn into_display(self) -> DurationDisplay;
 }
 
 impl Ext for Duration {
@@ -23,6 +26,9 @@ impl Ext for Duration {
     fn seconds(&self) -> u64 {
         self.as_secs() % 60
     }
+    fn into_display(self) -> DurationDisplay {
+        DurationDisplay(self)
+    }
 }
 
 /// builds a [Duration] from the given data
@@ -35,4 +41,20 @@ pub const fn duration_from_h_m_s_m(
     millis: u32,
 ) -> Duration {
     Duration::new(hours * 3600 + minutes * 60 + seconds, millis * 1_000_000)
+}
+
+/// a wrapper to hold a Duration for distplaing
+// TODO add configurations
+#[allow(clippy::module_name_repetitions)]
+pub struct DurationDisplay(std::time::Duration);
+impl std::fmt::Display for DurationDisplay {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "{}:{:0>2}.{:0>3}",
+            self.0.minutes(),
+            self.0.seconds(),
+            self.0.subsec_millis()
+        )
+    }
 }
